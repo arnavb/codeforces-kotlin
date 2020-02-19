9fbd2d21209816299c4ee8711952aa679ac2fab3
Arnav Borborah <arnavborborah11@gmail.com>
2020-02-18 21:03:21 -0500

    Solve 116A

---
+++ src/problem116A/Main.kt
@@ -0,0 +1,19 @@
+package problem116A
+
+fun main() {
+    val numStops = readLine()!!.toInt()
+
+    var currentlyOnBoard = 0
+    var capacity = 0
+    for (i in 0 until numStops) {
+        val (exiting, entering) = readLine()!!.split(' ').map { it.toInt() }
+        currentlyOnBoard += entering
+        currentlyOnBoard -= exiting
+
+        if (currentlyOnBoard > capacity) {
+            capacity = currentlyOnBoard
+        }
+    }
+
+    println(capacity)
+}
