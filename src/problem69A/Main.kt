0384bdbb613ed4b1a3c6d45e092a990700a7c50d
Arnav Borborah <arnavborborah11@gmail.com>
2020-02-18 20:50:58 -0500

    Solve 69A

---
+++ src/problem69A/Main.kt
@@ -0,0 +1,21 @@
+package problem69A
+
+fun main() {
+    val numLines = readLine()!!.toInt()
+
+    var xTotal = 0
+    var yTotal = 0
+    var zTotal = 0
+    for (i in 0 until numLines) {
+        val (x, y, z) = readLine()!!.split(' ').map { it.toInt() }
+        xTotal += x
+        yTotal += y
+        zTotal += z
+    }
+
+    if (xTotal == 0 && yTotal == 0 && zTotal == 0) {
+        println("YES")
+    } else {
+        println("NO")
+    }
+}
