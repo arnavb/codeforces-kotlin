634f646d175d124acb2dc8c16622f33a598bd145
Arnav Borborah <arnavborborah11@gmail.com>
2020-02-23 17:05:31 -0500

    Solve 479A

---
+++ src/problem479A/Main.kt
@@ -0,0 +1,13 @@
+package problem479A
+
+fun main() {
+    val numbers = listOf(readLine()!!, readLine()!!, readLine()!!).map { it.toInt() }
+
+    val possibilities = mutableListOf(numbers.reduce { acc, i -> acc * i }, numbers.sum())
+    possibilities.add(numbers[0] + numbers[1] * numbers[2])
+    possibilities.add((numbers[0] + numbers[1]) * numbers[2])
+    possibilities.add(numbers[0] * numbers[1] + numbers[2])
+    possibilities.add(numbers[0] * (numbers[1] + numbers[2]))
+
+    println(possibilities.max())
+}
