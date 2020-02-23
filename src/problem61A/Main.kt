c4cc883d3f4ea6771ee790aa568511e38fff8647
Arnav Borborah <arnavborborah11@gmail.com>
2020-02-23 13:00:32 -0500

    Solve 61A

---
+++ src/problem61A/Main.kt
@@ -0,0 +1,8 @@
+package problem61A
+
+fun main() {
+    val a = readLine()!!
+    val b = readLine()!!.toBigInteger(2)
+
+    println((a.toBigInteger(2) xor b).toString(2).padStart(a.length, '0'))
+}
