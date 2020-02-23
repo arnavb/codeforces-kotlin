2db64c719b55d191b4c9e2aac4fba5f257dda7c9
Arnav Borborah <arnavborborah11@gmail.com>
2020-02-23 17:14:44 -0500

    Solve 344A

---
+++ src/problem344A/Main.kt
@@ -0,0 +1,18 @@
+package problem344A
+
+fun main() {
+    val testCases = readLine()!!.toInt()
+
+    var groups = 0
+    var previousPair = -1
+
+    repeat(testCases) {
+        val currentPair = readLine()!!.toInt()
+        if (currentPair != previousPair) {
+            ++groups
+            previousPair = currentPair
+        }
+    }
+
+    println(groups)
+}
