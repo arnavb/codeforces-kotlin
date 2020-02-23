1e3c45c6358cef04e03ae538181d66a15e6e7dcd
Arnav Borborah <arnavborborah11@gmail.com>
2020-02-23 09:17:46 -0500

    Solve 785A

---
+++ src/problem785A/Main.kt
@@ -0,0 +1,20 @@
+package problem785A
+
+fun main() {
+    val numPolyhedrons = readLine()!!.toInt()
+
+    var sum = 0
+    for (i in 0 until numPolyhedrons) {
+        val currentShape = readLine()!!
+
+        sum += when (currentShape[0]) {
+            'T' -> 4
+            'C' -> 6
+            'O' -> 8
+            'D' -> 12
+            else -> 20
+        }
+    }
+
+    println(sum)
+}
