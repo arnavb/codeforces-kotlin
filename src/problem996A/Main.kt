40c90056a43c6abcf81ae5b5f8b08b80b1944723
Arnav Borborah <arnavborborah11@gmail.com>
2020-02-23 09:53:37 -0500

    Solve 996A

---
+++ src/problem996A/Main.kt
@@ -0,0 +1,19 @@
+package problem996A
+
+fun minimumBills(money: Long): Long {
+    var numberOfBills = 0L
+    var moneyTracker = money
+
+    for (bill in intArrayOf(100, 20, 10, 5, 1)) {
+        while (moneyTracker >= bill) {
+            moneyTracker -= bill
+            ++numberOfBills
+        }
+    }
+    return numberOfBills
+}
+
+fun main() {
+    val money = readLine()!!.toLong()
+    println(minimumBills(money))
+}
