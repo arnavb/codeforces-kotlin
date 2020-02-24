617897431deec512d45a87c65567d31856fc7a1f
Arnav Borborah <arnavborborah11@gmail.com>
2020-02-24 18:42:41 -0500

    Solve 1303A

---
+++ src/problem1303A/Main.kt
@@ -0,0 +1,33 @@
+package problem1303A
+
+fun numZerosToRemove(string: String): Int {
+    val totalZeros = string.count { it == '0' }
+
+    var numLeadingZeros = 0
+    for (char in string) {
+        if (char == '1') {
+            break
+        }
+        ++numLeadingZeros
+    }
+
+    var numTrailingZeros = 0
+    for (i in string.length - 1 downTo 0) {
+        if (string[i] == '1') {
+            break
+        }
+        ++numTrailingZeros
+    }
+
+    val result = totalZeros - numLeadingZeros - numTrailingZeros
+
+    return if (result > 0) result else 0
+}
+
+fun main() {
+    val testCases = readLine()!!.toInt()
+
+    repeat(testCases) {
+        println(numZerosToRemove(readLine()!!))
+    }
+}
