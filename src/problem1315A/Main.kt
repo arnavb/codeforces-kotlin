ec55d9f107fe494e977d35b536a19b3c1ed8625d
Arnav Borborah <arnavborborah11@gmail.com>
2020-02-25 17:38:55 -0500

    Solve 1315A
    Tried a new approach with primitive arrays and Scanner.

---
+++ src/problem1315A/Main.kt
@@ -0,0 +1,23 @@
+package problem1315A
+
+import java.util.Scanner
+
+fun main() = with(Scanner(System.`in`)) {
+    val testCases = nextInt()
+
+    repeat(testCases) {
+        val a = nextInt()
+        val b = nextInt()
+        val x = nextInt()
+        val y = nextInt()
+
+        val possibleAreas = IntArray(4)
+
+        possibleAreas[0] = x * b
+        possibleAreas[1] =  (a - x - 1) * b
+        possibleAreas[2] = y * a
+        possibleAreas[3] = (b - y - 1) * a
+
+        println(possibleAreas.max())
+    }
+}
